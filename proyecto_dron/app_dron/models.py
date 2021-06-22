be0efa9cb56e558c81977c132da6b738ffa67122
matias4g63t <mati.bravom@duocuc.cl>
2021-06-22 04:47:55 -0400

    Configuracion e integracion de oracle, creacion de modelos

--- proyecto_dron/app_dron/models.py
+++ proyecto_dron/app_dron/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    idCategoria = models.IntegerField(primary_key=True,verbose_name='Id de categoria')
+    nombreCategoria = models.CharField(max_length=50, verbose_name='Nombre categoria')
+    
+    def __str__(self):
+        return self.nombreCategoria
+    
+class Aeronave(models.Model):
+    numeroSerie = models.CharField(max_length=8, primary_key=True, verbose_name='Numero serie')
+    marca = models.CharField(max_length=20, verbose_name='Marca Aeronave')
+    modelo = models.CharField(max_length=20, verbose_name='Modelo de la aeronave')
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.patente
+    
--- proyecto_dron/proyecto_dron/settings.py
+++ proyecto_dron/proyecto_dron/settings.py
@@ -76,9 +76,16 @@ WSGI_APPLICATION = 'proyecto_dron.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '127.0.0.1:1521/xe',
+        'USER': 'c##experiencia3',
+        'PASSWORD': '123456',
+        'TEST': {
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp',
+        },
+    },
 }
 
 
